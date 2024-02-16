158668942b7129100fda0f280e8a687a84380e7a
chrajesh379 <155586966+chrajesh379@users.noreply.github.com>
2024-02-16 09:26:37 -0800

    Update data_preprocessing_tools.py

--- DataProcessing/data_preprocessing_tools.py
+++ DataProcessing/data_preprocessing_tools.py
@@ -7,6 +7,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+#--------------------------------------
+print("-----------------------------pima-indians-diabetes-database start------------------------------")
+
+# Load the dataset
+#dataset = datasets.load_p
+dataset = pd.read_csv('pima-indians-diabetes.csv')
+#dataset = pd.read_csv("/kaggle/input/pima-indians-diabetes-database/diabetes.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+# Identify missing data (assumes that missing data is represented as NaN)
+print(dataset.columns)
+missing_data = dataset.isnull().sum()
+# Print the number of missing entries in each column
+print(f"Missing data : \n {missing_data}")
+# Configure an instance of the SimpleImputer class
+imputer = SimpleImputer(missing_values=np.nan, strategy = "mean")
+# Fit the imputer on the DataFrame
+imputer.fit(X[:, :-1])
+# Apply the transform to the DataFrame
+X[:, :-1]=imputer.transform(X[:, :-1])
+#Print your updated matrix of features
+print(X)
+print("-----------------------------pima-indians-diabetes-database End------------------------------")
+#--------------------------------------
 
 dataset = pd.read_csv("Data.csv")
 #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.iloc.html#pandas.DataFrame.iloc
@@ -19,9 +47,29 @@ print(y)
 #print(iris)
 #dataset = pd.DataFrame(iris.data)
 #print(dataset)
+
+###############################################################
+# Taking Care of Missing Data for Numerical Data
+###############################################################
+# Simpleimputer is used to populate Missing Values
 imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
 # fit () — This method goes through the training data, calculates the parameters (like mean (μ) and standard deviation (σ) in StandardScaler class ) and saves them as internal objects.
 imputer.fit(X[:, 1:3])
 #transform() — The parameters generated using the fit() method are now used and applied to the training data to update them.
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 print(X)
+
+###############################################################
+# Encoding Categorical Data
+###############################################################
+# Encoding Independent Variables
+
+ct = ColumnTransformer(transformers=[('encoding',OneHotEncoder(),[0])], remainder='passthrough')
+X=np.array(ct.fit_transform(X))
+
+# Encoding Dependent Variables
+le = LabelEncoder()
+y=le.fit_transform(y)
+print(X)
+print(y)
+print(type(X))
