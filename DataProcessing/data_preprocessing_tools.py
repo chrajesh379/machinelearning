f70ecfb76e9a721601ed0eafbe3dbfc1d6612899
chrajesh379 <155586966+chrajesh379@users.noreply.github.com>
2024-02-16 09:31:00 -0800

    Update data_preprocessing_tools.py

--- DataProcessing/data_preprocessing_tools.py
+++ DataProcessing/data_preprocessing_tools.py
@@ -36,6 +36,35 @@ print(X)
 print("-----------------------------pima-indians-diabetes-database End------------------------------")
 #--------------------------------------
 
+print("-----------------------------------titanic csv start---------------")
+# Load the dataset
+df = pd.read_csv('titanic.csv')
+
+# Identify the categorical data
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1]
+
+categorical_features = ['Sex', 'Embarked', 'Pclass']
+
+# Implement an instance of the ColumnTransformer class
+ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),categorical_features)], remainder='passthrough')
+
+# Apply the fit_transform method on the instance of ColumnTransformer
+X = ct.fit_transform(df)
+
+# Convert the output into a NumPy array
+X = np.array(X)
+
+# Use LabelEncoder to encode binary categorical data
+le = LabelEncoder()
+y = le.fit_transform(df['Survived'])
+
+# Print the updated matrix of features and the dependent variable vector
+print(X)
+print(y)
+
+print("-----------------------------------titanic csv End---------------")
+
 dataset = pd.read_csv("Data.csv")
 #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.iloc.html#pandas.DataFrame.iloc
 #dataframe.iloc[row, column]
